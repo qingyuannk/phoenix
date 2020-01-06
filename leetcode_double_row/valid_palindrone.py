08c87a98ef525882270245b0ee3e8119703aa9ad
qingyuanli <lqingy928@gmail.com>
2020-01-06 16:02:37 +0800

    valid_palindrone.py

--- leetcode_double_row/valid_palindrone.py
+++ leetcode_double_row/valid_palindrone.py
@@ -8,3 +8,29 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         s=[*filter(str.isalnum,s.lower())]
         return s == s[::-1]
+'''
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        n= len(s)
+        if(n==0):
+            return True
+        a =[]
+        boolen = True
+        for i in s.lower():
+            if('a'<= i<='z' or '0'<= i <= '9'):
+                a.append(i)
+        L = 0
+        R = len(a)-1
+        while(L<R):
+            if(a[L] == a[R]):
+                L =L + 1
+                R =R - 1
+            else:
+                boolen = False
+                break
+        return boolen
+'''
