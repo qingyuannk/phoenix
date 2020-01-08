f19496760e828e673ec0039459475e66be3e9870
qingyuanli <lqingy928@gmail.com>
2020-01-08 19:52:18 +0800

    Listed_List_Cycle.py

---
+++ leetcode_double_row/Listed_List_Cycle.py
@@ -0,0 +1,29 @@
+"""
+Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if(not head):
+            return False
+        L= head
+        R= head
+        while(R and R.next):
+            L = L.next
+            R = R.next.next
+            if(L==R):
+                return True
+        return Fal
